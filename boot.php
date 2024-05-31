0686056a44d468770da7ed4a67211970db2fc5b5
Thomas Skerbis <thomas.skerbis@mac.com>
2024-05-31 22:50:48 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -4,6 +4,6 @@ if (rex::isBackend() && rex::getUser()) {
  if (!rex_addon::get('minibar')->isAvailable()) {
             return '';
         }  
-   rex_view::addJsFile(rex_addon::get('quick_navigation')->getAssetsUrl('MinibarTimeTrack.js'));
+   rex_view::addJsFile(rex_addon::get('minibar_time_track')->getAssetsUrl('MinibarTimeTrack.js'));
 rex_minibar::getInstance()->addElement(new FriendsOfRedaxo\MinibarTimeTrack\MinibarTimeTrack());
 }
